bc17711c5e2067f439140c95109d5a20303d8953
DanielWLam <lwd931227@126.com>
2018-01-26 18:23:37 +0800

    去掉模板内容

---
+++ bin/dwl-react-cli-init.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const path = require('path')
+
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
+}
--- index.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-import Tab from './components/Tab.jsx'
-
-render(<Tab />, document.getElementById('app'))
-
-
-// console.log(123)
