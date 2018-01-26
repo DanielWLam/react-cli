1380970bee12e158884e13452fa75e7123279f15
DanielWLam <lwd931227@126.com>
2018-01-26 19:00:00 +0800

    下载脚本

--- bin/dwl-react-cli-init.js
+++ bin/dwl-react-cli-init.js
@@ -1,6 +1,36 @@
 #!/usr/bin/env node
 const path = require('path')
+const ora = require('ora')
+const shell = require('shelljs')
 
-function resolve (dir) {
-  return path.resolve(__dirname, dir)
+function clone (dir) {
+  return new Promise((resolve, reject) => {
+    const spinner = ora('Downloading template from https://github.com/DanielWLam/react-cli-template').start()
+    const name = 'react-cli-template'
+    const url = 'https://github.com/DanielWLam/react-cli-template'
+    const branch = 'master'
+    shell.rm('-rf', dir)
+    shell.exec(`git clone ${url} -b ${branch} --single-branch ${dir}`, {
+      silent: true
+    }, (code, stdout, stderr) => {
+      if (code === 0) {
+        shell.rm('-rf', path.join(dir, '.git'))
+        spinner.succeed('Download template successed')
+        resolve()
+      } else {
+        shell.rm('-rf', dir)
+        spinner.fail('Download template fail with reason: ', stderr)
+        reject()
+      }
+    })
+  })
+}
+
+module.exports = function (dir) {
+  // console.log(dir)
+  try {
+    clone(dir)
+  } catch (e) {
+    console.log(e)
+  }
 }
